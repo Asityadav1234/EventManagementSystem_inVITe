8a36a2e400bbe06d2b90747682745ea22011673e
Asityadav <asit50154@gmail.com>
2025-04-12 17:01:18 +0530

    Update admincontroller.js

--- server/controllers/admincontroller.js
+++ server/controllers/admincontroller.js
@@ -13,20 +13,37 @@ const setAdmin = async (req, res) => {
     const token = await jwt.sign(payload, secret);
 
     const new_admin = new Admin({
-        admin_id: token,
+        pass: req.body.password,
         email: req.body.email,
         name: req.body.name,
-        pass: req.body.password,
+        admin_id: token,
     });
 
     await new_admin.save((error, success) => {
         if (error) console.log(error);
-        else console.log("Saved::New Admin::credentials.");
+        else console.log("Saved");
     });
 
     res.status(200).send({ msg: "Credentials Added" });
 };
 
+const adminAuth = async (req, res) => {
+    const Email = req.body.email;
+    const Pass = req.body.password;
+
+    Admin.find({ email: Email }, async function (err, docs) {
+        if (docs.length === 0) {
+            return res.status(400).send({ msg: "Admin access denied" });
+        } else if (Pass === docs[0].pass) {
+            res.status(200).send({
+                msg: "Success",
+                admin_token: docs[0].admin_id,
+            });
+        } else {
+            return res.status(400).send({ msg: "Email or Password is wrong" });
+        }
+    });
+};
 require("./models/otpAuth");
 require("./models/user");
 require("./models/admin");
