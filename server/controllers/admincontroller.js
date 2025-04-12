9ded6b139521892f55dd46dea3e4722cb141a9d4
Sarvesh5579 <143869659+Sarvesh5579@users.noreply.github.com>
2025-04-12 16:59:00 +0530

    Update admincontroller.js

--- server/controllers/admincontroller.js
+++ server/controllers/admincontroller.js
@@ -4,29 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const setAdmin = async (req, res) => {
-    const secret = JWT_SECRET;
-    const payload = {
-        email: req.body.email,
-    };
-
-    const token = await jwt.sign(payload, secret);
-
-    const new_admin = new Admin({
-        admin_id: token,
-        email: req.body.email,
-        name: req.body.name,
-        pass: req.body.password,
-    });
-
-    await new_admin.save((error, success) => {
-        if (error) console.log(error);
-        else console.log("Saved::New Admin::credentials.");
-    });
-
-    res.status(200).send({ msg: "Credentials Added" });
-};
-
 
 require("./models/otpAuth");
 require("./models/user");
