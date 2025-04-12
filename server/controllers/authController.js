3389f0f6997bd51d68a7a3530c0461b6c50b1b4c
vikassingh20 <2023ucp1582@mnit.ac.in>
2025-04-12 23:18:44 +0530

    Create authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const OtpAuth = require("../models/otpAuth");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
+const otpGenerator = require("otp-generator");
+
+const { sendSMS } = require("./smsController");
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+console.log("in auth - ", JWT_SECRET);
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
