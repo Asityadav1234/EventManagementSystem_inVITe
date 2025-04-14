f2e7b0b70e0a93a1e725eefd8e2341203221ed95
vikassingh20 <2023ucp1582@mnit.ac.in>
2025-04-14 14:16:32 +0530

    other Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -74,3 +74,63 @@ const signIn = async (req, res) => {
         }
     });
 };
+
+// route - http://localhost:5000/user/signup
+const signUp = async (req, res) => {
+    const Email = req.body.email;
+
+    //validating whether user already exists or not
+
+    User.find({ email: Email }, async function (err, docs) {
+        if (docs.length !== 0) {
+            return res.status(400).send({
+                msg: "This Email ID is already registered. Try Signing In instead!",
+            });
+        } else {
+            //clearing otp auth table
+            try {
+                await OtpAuth.deleteMany({ email: Email }, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Users deleted successfully");
+                    }
+                });
+            } catch (e) {
+                console.log(e);
+            }
+
+            // generate otp for new user
+            const OTP = otpGenerator.generate(6, {
+                digits: true,
+                upperCaseAlphabets: false,
+                specialChars: false,
+                lowerCaseAlphabets: false,
+            });
+
+            const otp = {
+                email: Email,
+                otp: OTP,
+            };
+            console.log("Before hashing: ", otp);
+
+            sendSMS(Email, otp.otp);
+
+            //encrypting the otp and then saving to Otp_table
+            const salt = await bcrypt.genSalt(10);
+            otp.otp = await bcrypt.hash(otp.otp, salt);
+
+            const newUserLogin = new OtpAuth({
+                email: otp.email,
+                otp: otp.otp,
+            });
+
+            newUserLogin.save((error, success) => {
+                if (error) console.log(error);
+                else console.log("Saved::otp::ready for validation");
+            });
+
+            return res.status(200).send({ msg: "Otp sent successfully!" });
+        }
+    });
+};
