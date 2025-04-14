30c43b016428dce869bd02db974aee6ce2314d03
vikassingh20 <2023ucp1582@mnit.ac.in>
2025-04-14 15:45:54 +0530

    other Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -134,3 +134,102 @@ const signUp = async (req, res) => {
         }
     });
 };
+
+// route - http://localhost:5000/user/signin/verify
+const verifyLogin = async (req, res) => {
+    const Email = req.body.email;
+    const inputOtp = req.body.otp;
+
+    OtpAuth.find({ email: Email }, async function (err, docs) {
+        if (docs.length === 0) {
+            return res
+                .status(400)
+                .send({ msg: "The OTP expired. Please try again!" });
+        } else {
+            const generatedOtp = docs[0].otp;
+
+            const validUser = await bcrypt.compare(inputOtp, generatedOtp);
+
+            if (Email === docs[0].email && validUser) {
+                User.find({ email: Email }, async function (err, user) {
+                    console.log(user);
+                    res.status(200).send({
+                        msg: "Sign-In successful!",
+                        user_id: user[0].user_token,
+                    });
+                });
+            } else {
+                return res
+                    .status(406)
+                    .send({ msg: "OTP does not match. Please try again!" });
+            }
+        }
+    });
+};
+
+// route - http://localhost:5000/user/signup/verify
+const verifyOtp = async (req, res) => {
+    const number = req.body.contactNumber;
+    const inputOtp = req.body.otp;
+    const Email = req.body.email;
+    const name = req.body.username;
+    const regNumber = req.body.regNumber;
+
+    OtpAuth.find({ email: Email }, async function (err, docs) {
+        if (docs.length === 0) {
+            return res.status(400).send("The OTP expired. Please try again!");
+        } else {
+            const generatedOtp = docs[0].otp;
+
+            const validUser = await bcrypt.compare(inputOtp, generatedOtp);
+
+            if (Email === docs[0].email && validUser) {
+                const secret = JWT_SECRET;
+                const payload = {
+                    email: req.body.email,
+                };
+                const token = jwt.sign(payload, secret);
+
+                //saving new user
+                const newUser = new User({
+                    user_token: token,
+                    reg_number: regNumber,
+                    username: name,
+                    email: Email,
+                    contactNumber: number,
+                });
+
+                newUser.save((error, success) => {
+                    if (error) console.log(error);
+                    else console.log("Signup successful: ", newUser);
+                });
+
+                OtpAuth.deleteMany({ email: Email }, async function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(`OTP table for ${Email} cleared.`);
+                    }
+                });
+
+                return res
+                    .status(200)
+                    .send({
+                        msg: "Account creation successful!",
+                        user_id: token,
+                    });
+            } else {
+                return res
+                    .status(400)
+                    .send({ msg: "OTP does not match. Please try again!" });
+            }
+        }
+    });
+};
+
+module.exports = {
+    signUp,
+    verifyOtp,
+    signIn,
+    verifyLogin,
+};
